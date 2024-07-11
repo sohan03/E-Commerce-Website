a861926a04831630a78f56df60dea9b3d76f9b56
Sohan K <94063015+sohan03@users.noreply.github.com>
2024-07-11 12:49:17 +0530

    Update AddProduct.jsx

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -136,7 +136,7 @@ const AddProduct = () => {
  
             let formData = new FormData();
             formData.append('product', image);
-            await fetch('http://localhost:4000/upload',{
+            await fetch('https://e-commerce-website-e5ns.onrender.com/upload',{
                 method:'POST',
                 headers:{
                     Accept:'application/json',
@@ -146,7 +146,7 @@ const AddProduct = () => {
             if(responseData.success){
                 product.image = responseData.image_url;
                 console.log(product);
-                await fetch('http://localhost:4000/addproduct',{
+                await fetch('https://e-commerce-website-e5ns.onrender.com/addproduct',{
                     method:'POST',
                     headers:{
                         Accept:'application/json',
