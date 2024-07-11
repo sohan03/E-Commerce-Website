0a337c8f3c323c6e24b1cbfd07f1110fa6e330ce
Sohan K <94063015+sohan03@users.noreply.github.com>
2024-07-11 11:35:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,13 @@ app.use(express.json());
 app.use(cors());
  
 // Database MongoDB Connection
-mongoose.connect("mongodb+srv://sohankaran35:sohan%231237@cluster0.yhgyntv.mongodb.net/e-commerce");
+mongoose.connect("mongodb+srv://sohankaran35:sohan%231237@cluster0.yhgyntv.mongodb.net/e-commerce", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true,
+  sslValidate: true,
+  tlsAllowInvalidCertificates: true // Only for development; not recommended for production
+})
  
 app.get("/", (req, res) => {
     res.send("Express App is Running");
